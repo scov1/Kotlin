096857d2d2d8472c2fdc70c36876deabfa6bd7f3
seda <seda@MacBook-Pro-seda.local>
2020-09-03 10:22:34 +0600

    selfie

--- app/src/main/java/org/step/callphonecamera/MainActivity.kt
+++ app/src/main/java/org/step/callphonecamera/MainActivity.kt
@@ -12,40 +12,24 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var img : ImageView
-    lateinit var btnImg : Button
-    lateinit var btnCall : Button
-    val phoneNumber = "77029552207"
-    val REQUEST_PHONE_CALL = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        img = findViewById(R.id.action_img)
-        btnImg = findViewById(R.id.action_btn_img)
-        btnCall = findViewById(R.id.action_btn_phone)
+
 
        // requestPermissions{android.Manifest.permission.CAMERA}
 
-        btnImg.setOnClickListener{
+        button_activity_main_img.setOnClickListener{
             if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),123)
             }else{
                 startCamera()
-            }
-        }
-
-        btnCall.setOnClickListener{
-
-            if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE),REQUEST_PHONE_CALL)
-            }else{
-                startCall()
             }
         }
     }
@@ -56,28 +40,12 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent,123)
     }
 
-    private fun startCall() {
-        val intent = Intent(Intent.ACTION_CALL)
-        intent.data = Uri.parse("tel: $phoneNumber")
-        startActivity(intent)
-    }
-
-
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode==123){
             var bmp = data?.extras?.get("data") as Bitmap
-            img.setImageBitmap(bmp)
+            imageView_activity_main_img.setImageBitmap(bmp)
         }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if(requestCode === REQUEST_PHONE_CALL)startCall()
     }
 }
